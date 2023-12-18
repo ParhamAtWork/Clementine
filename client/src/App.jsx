6d975668dca0880a5134dec9999e268891e0e0c3
Boscoe <garrett.boscoe@fiserv.com>
2023-12-18 10:20:45 -0500

    added logo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import clementineLogo from '../public/clementine.png'
 import './App.css'
 
 function App() {
@@ -10,13 +10,13 @@ function App() {
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={clementineLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Clementine</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
