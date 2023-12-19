0ddbd0513dca09aaa6300d6e6205b320559327cf
Parham Saniei <parham.saniei@penta.com>
2023-12-19 11:18:28 -0500

    checkpoint

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import clementineLogo from '../public/clementine.png'
 import './App.css'
-import Example from './layouts/Dashboard'
+import Example from './pages/Dashboard'
 
 function App() {
   return (
