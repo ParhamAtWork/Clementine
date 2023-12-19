3dc8c2b7c463e4d662ba37d95f7ad2fd531b1d19
JoeSchmitt-2 <84737443+JoeSchmitt-2@users.noreply.github.com>
2023-12-19 16:20:06 -0500

    auth0 finalized

--- routes/db.js
+++ routes/db.js
@@ -1,5 +1,7 @@
 var express = require("express");
 var router = express.Router();
+const { requiresAuth } =require('express-openid-connect') // use this to enforce authentication on routing
+const axois = require('axios')
 
 router.get("/", (req, res) => {
   console.log(req.oidc.isAuthenticated());
@@ -7,6 +9,26 @@ router.get("/", (req, res) => {
     title: "express demo",
     isAuthenticated: req.oidc.isAuthenticated(),
     user: req.oidc.user,
+  });
+});
+
+router.get("/secured", requiresAuth(), async (req, res) => {
+  let data = {}
+
+  const {token_type, access_token} = req.oidc.accessToken
+
+  try {
+    const apiResponse = await axios.get('http://localhost:5000/public')
+    data = apiResponse.data
+  } catch(e) {
+
+  }
+  console.log(req.oidc.isAuthenticated());
+  res.render("secured", {
+    title: "Secure Page",
+    isAuthenticated: req.oidc.isAuthenticated(),
+    user: req.oidc.user,
+    data
   });
 });
 
