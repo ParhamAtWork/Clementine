f1e4c243c131f94937dc356080644f075b73d731
Parham Saniei <parham.saniei@penta.com>
2023-12-26 10:17:24 -0500

    Co-authored-by: Saniei, Parham (Berkeley Heights) <parham.saniei@fiserv.com>

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,41 +1,56 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { withAuthenticationRequired } from "@auth0/auth0-react";
-import { Auth0Provider } from "@auth0/auth0-react";
-import PropTypes from "prop-types";
-import { useAuth0 } from "@auth0/auth0-react";
-import "./assets/clementinesv.svg";
-import "./App.css";
-import Dashboard from "./pages/tenantsPages/Dashboard";
-import LDashboard from "./pages/landlordPages/LDashboard";
-import WelcomePage from "./pages/WelcomePage";
-
+import {
+	BrowserRouter as Router,
+	Route,
+	Routes,
+	BrowserRouter,
+} from 'react-router-dom';
+import { withAuthenticationRequired } from '@auth0/auth0-react';
+import { Auth0Provider } from '@auth0/auth0-react';
+import PropTypes from 'prop-types';
+import { useAuth0 } from '@auth0/auth0-react';
+import { useNavigate } from 'react-router-dom';
+import './assets/clementinesv.svg';
+import './App.css';
+import Dashboard from './pages/tenantsPages/Dashboard';
+import LDashboard from './pages/landlordPages/LDashboard';
+import WelcomePage from './pages/WelcomePage';
 
 function App() {
-  const { user, isAuthenticated, isLoading } = useAuth0();
+	const { user, isAuthenticated, isLoading } = useAuth0();
 
-  console.log(isAuthenticated)
+	console.log(isAuthenticated);
 
-  return (
-    <>
-      <Auth0Provider
-        domain="dev-wyvx8c2jswjwvuxo.us.auth0.com"
-        clientId="HSjhZMZWxMGNxjM5DtBD5hTxHH0Fm0ql"
-        authorizationParams={{
-          redirect_uri: "http://localhost:3000/landlord-dashboard"
-        }}
-      >
-        <Router>
-          <Routes>
-            <Route exact path="/" element={<WelcomePage />} />
-            isAuthenticated && (
-            <Route path="/landlord-dashboard" element={<LDashboard />} />
-            <Route path="/tenant-dashboard" element={<Dashboard />} />
-            )
-          </Routes>
-        </Router>
-      </Auth0Provider>
-    </>
-  );
+	return (
+		<>
+			<Auth0Provider
+				domain='dev-wyvx8c2jswjwvuxo.us.auth0.com'
+				clientId='HSjhZMZWxMGNxjM5DtBD5hTxHH0Fm0ql'
+				authorizationParams={{
+					redirect_uri: 'http://localhost:3000/landlord-dashboard',
+				}}
+			>
+				<Router>
+					<Routes>
+						<Route
+							exact
+							path='/'
+							element={<WelcomePage />}
+						/>
+						isAuthenticated && (
+						<Route
+							path='/landlord-dashboard'
+							element={<LDashboard />}
+						/>
+						<Route
+							path='/tenant-dashboard'
+							element={<Dashboard />}
+						/>
+						)
+					</Routes>
+				</Router>
+			</Auth0Provider>
+		</>
+	);
 }
 
 export default App;
--- client/src/pages/WelcomePage.jsx
+++ client/src/pages/WelcomePage.jsx
@@ -1,54 +1,83 @@
-import clementineLogo from "../assets/clementinesv.svg";
-import "./WelcomePage.css";
-import { useAuth0 } from "@auth0/auth0-react";
+import clementineLogo from '../assets/clementinesv.svg';
+import './WelcomePage.css';
+import { useAuth0 } from '@auth0/auth0-react';
+import { Auth0Provider } from '@auth0/auth0-react';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import.meta.env;
 
 export default function Example() {
-  const { loginWithRedirect } = useAuth0();
+	const { loginWithRedirect } = useAuth0();
+	const navigate = useNavigate();
 
-  return (
-    <div>
-      <header className="absolute inset-x-0 top-0 z-50">
-        <nav
-          className="flex items-center justify-center p-6 lg:px-8"
-          aria-label="Global"
-        >
-          {/* Flex container for logo and title */}
-          <div className="flex flex-col items-center lg:flex-row"></div>
-          <div className="hidden lg:flex lg:gap-x-12"></div>
-          <div className="hidden lg:flex lg:flex-1 lg:justify-end"></div>
-        </nav>
-      </header>
+	// const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+	// const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+	// const redirectUri = process.env.REACT_APP_AUTH0_CALLBACK_URL;
 
-      <div className="relative isolate px-6 pt- lg:px-8">
-        <div
-          className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-          aria-hidden="true"
-        ></div>
-        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-          <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-            <img
-              className="h-20 w-auto" // Adjust size as needed
-              src={clementineLogo}
-              alt="Clementine"
-            />
-            <h1 className="text-orange mt-3">Clementine</h1>
-          </div>
-          <div className="text-center">
-            <h4 className="text-3xl font-bold tracking-tight text-gray-900">
-              The rent payment solution
-            </h4>
-            <div className="mt-10 flex items-center justify-center gap-x-6">
-              <button onClick={() => loginWithRedirect()} className="bg-[#558540] text-stone hover:text-stone w-auto px-10 py-2 border-2 border-[#000]">
-                  Log in
-              </button>
-            </div>
-          </div>
-        </div>
-        <div
-          className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-          aria-hidden="true"
-        ></div>
-      </div>
-    </div>
-  );
+	// const onRedirectCallback = (appState) => {
+	// 	navigate(appState?.returnTo || window.location.pathname);
+	// };
+
+	// if (!(domain && clientId && redirectUri)) {
+	// 	return null;
+	// }
+
+	return (
+		// <Auth0Provider
+		// 	domain={domain}
+		// 	clientId={clientId}
+		// 	authorizationParams={{
+		// 		redirect_uri: redirectUri,
+		// 	}}
+		// 	onRedirectCallback={onRedirectCallback}
+		// >
+		<div>
+			<header className='absolute inset-x-0 top-0 z-50'>
+				<nav
+					className='flex items-center justify-center p-6 lg:px-8'
+					aria-label='Global'
+				>
+					{/* Flex container for logo and title */}
+					<div className='flex flex-col items-center lg:flex-row'></div>
+					<div className='hidden lg:flex lg:gap-x-12'></div>
+					<div className='hidden lg:flex lg:flex-1 lg:justify-end'></div>
+				</nav>
+			</header>
+
+			<div className='relative isolate px-6 pt- lg:px-8'>
+				<div
+					className='absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80'
+					aria-hidden='true'
+				></div>
+				<div className='mx-auto max-w-2xl py-32 sm:py-48 lg:py-56'>
+					<div className='hidden sm:mb-8 sm:flex sm:justify-center'>
+						<img
+							className='h-20 w-auto' // Adjust size as needed
+							src={clementineLogo}
+							alt='Clementine'
+						/>
+						<h1 className='text-orange mt-3'>Clementine</h1>
+					</div>
+					<div className='text-center'>
+						<h4 className='text-3xl font-bold tracking-tight text-gray-900'>
+							The rent payment solution
+						</h4>
+						<div className='mt-10 flex items-center justify-center gap-x-6'>
+							<button
+								onClick={() => loginWithRedirect()}
+								className='bg-[#558540] text-stone hover:text-stone w-auto px-10 py-2 border-2 border-[#000]'
+							>
+								Log in
+							</button>
+						</div>
+					</div>
+				</div>
+				<div
+					className='absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]'
+					aria-hidden='true'
+				></div>
+			</div>
+		</div>
+		// </Auth0Provider>
+	);
 }
