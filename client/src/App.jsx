4bd32c447ac2e0a8ba0fa7aad98b0176075c22d2
JoeSchmitt-2 <84737443+JoeSchmitt-2@users.noreply.github.com>
2024-01-01 17:39:57 -0500

    AUTHENTICATIONS

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,51 +1,47 @@
 /* eslint-disable no-unused-vars */
 import {
-	BrowserRouter as Router,
-	Route,
-	Routes,
-	BrowserRouter,
-	json,
-} from 'react-router-dom';
-import axios from 'axios';
-import { withAuthenticationRequired } from '@auth0/auth0-react';
-import { Auth0Provider } from '@auth0/auth0-react';
-import PropTypes from 'prop-types';
-import { useAuth0 } from '@auth0/auth0-react';
-import { useNavigate } from 'react-router-dom';
-import './assets/clementinesv.svg';
-import './App.css';
-import Dashboard from './pages/tenantsPages/Dashboard';
-import LDashboard from './pages/landlordPages/LDashboard';
-import WelcomePage from './pages/WelcomePage';
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  BrowserRouter,
+  json,
+} from "react-router-dom";
+import axios from "axios";
+import { withAuthenticationRequired } from "@auth0/auth0-react";
+import { Auth0Provider } from "@auth0/auth0-react";
+import PropTypes from "prop-types";
+import { useAuth0 } from "@auth0/auth0-react";
+import { useNavigate } from "react-router-dom";
+import "./assets/clementinesv.svg";
+import "./App.css";
+import Dashboard from "./pages/tenantsPages/Dashboard";
+import LDashboard from "./pages/landlordPages/LDashboard";
+import WelcomePage from "./pages/WelcomePage";
 
 function App() {
-	const { user, isAuthenticated, isLoading } = useAuth0();
+  const { user, isAuthenticated, isLoading } = useAuth0();
+  const navigate = useNavigate();
 
-	console.log(isAuthenticated);
-	const tenantKey = user;
-	console.log('tenantKey: ', tenantKey?.nickname);
+  console.log(isAuthenticated);
+  const tenantKey = user;
+  console.log("tenantKey: ", tenantKey?.nickname);
 
-	return (
-		<>
-			<Routes>
-				<Route
-					exact
-					path='/'
-					element={<WelcomePage />}
-				/>
-				isAuthenticated && (
-				<Route
-					path='/landlord-dashboard'
-					element={<LDashboard />}
-				/>
-				<Route
-					path='/tenant-dashboard'
-					element={<Dashboard />}
-				/>
-				)
-			</Routes>
-		</>
-	);
+  // Check if the user is authenticated and if tenantKey?.nickname is "joseph.schmitt1"
+  // TODO: CHANGE THIS TO BOBBY
+  if (isAuthenticated && tenantKey?.nickname === "joseph.schmitt1") {
+    // Redirect to /tenant-dashboard
+    navigate('/tenant-dashboard');
+  }
+
+  return (
+    <>
+      <Routes>
+        <Route exact path='/' element={<WelcomePage />} />
+        <Route path='/landlord-dashboard' element={<LDashboard />} />
+        <Route path='/tenant-dashboard' element={<Dashboard />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
